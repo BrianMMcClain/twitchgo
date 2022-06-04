81c8b50e44ff39f0d839238cc9f5e6447863e158
Brian McClain <brianmmcclain@gmail.com>
2022-06-03 23:13:11 -0400

    Read input to choose which chat to connect to

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
+	"strconv"
 
 	twitchbuddy "github.com/brianmmcclain/twitch-buddy-go/lib"
 )
@@ -14,7 +18,23 @@ func main() {
 	fmt.Printf("Hello, %s!\n", u.DisplayName)
 
 	streams := twitch.GetFollowedStreams()
-	for _, s := range streams {
-		fmt.Printf("%s (%d - %s): %s\n", s.UserName, s.ViewerCount, s.GameName, s.Title)
+	for i, s := range streams {
+		fmt.Printf("%d: %s (%d - %s): %s\n", i+1, s.UserName, s.ViewerCount, s.GameName, s.Title)
 	}
+	fmt.Print("Choose a chat to connect to: ")
+
+	reader := bufio.NewReader(os.Stdin)
+	inS, _, err := reader.ReadLine()
+	if err != nil {
+		log.Fatalf("Error reading input %s", err)
+	}
+
+	in, err := strconv.Atoi(string(inS))
+	if err != nil {
+		log.Fatalf("Error parsing input: %s", err)
+	} else if in <= 0 || in > len(streams) {
+		log.Fatalf("Invalid input: %d", in)
+	}
+
+	fmt.Printf("Connecting to %s . . .\n", streams[in-1].UserName)
 }
